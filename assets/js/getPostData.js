7997346b6a6dbc638eac289e0589231e133e50ff
Max Antonucci <maxx1128@gmail.com>
2020-05-06 21:59:53 -0400

    Cut external posts from gathered post data

--- assets/js/getPostData.js
+++ assets/js/getPostData.js
@@ -3,13 +3,8 @@
 
   const setRawData = data => PostData.raw = data;
 
-  const setInternalData = (data) => {
-    PostData.internal = data.filter(item => !item.link);
-  };
-
   PostData.init = (data) => {
     setRawData(data);
-    setInternalData(data);
   };
 
   window.PostData = PostData;
--- assets/js/randomPostButton.js
+++ assets/js/randomPostButton.js
@@ -1,17 +1,10 @@
 (function() {
-  const posts = window.PostData.internal,
+  const posts = window.PostData.raw,
         randomPost = posts[Math.floor(Math.random() * posts.length)],
         linkElement = document.getElementById('random-link');
 
   if (linkElement) {
     linkElement.classList.remove('hidden');
-
-    if (randomPost.link) {
-      linkElement.setAttribute('href', randomPost.link);
-      linkElement.setAttribute('target', '_blank');
-      linkElement.setAttribute('rel', 'noopener');
-    } else {
-      linkElement.setAttribute('href', randomPost.url);
-    }
+    linkElement.setAttribute('href', randomPost.url);
   }
 })();
